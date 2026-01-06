f7c5ead8f86420ccb97dcef9d23facd9a4c0ecee
Alexey Yakauleu iMac Home <yakovlev@yakovlevdev.com>
2026-01-06 23:15:03 +0300

    feat: implement google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,19 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <meta name="apple-mobile-web-app-title" content="Орша Шар" />
       </head>
       <body className="app">
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N8RX75N9');`}
+        </Script>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html:
+              '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N8RX75N9" height="0" width="0" style="display:none;visibility:hidden"></iframe>'
+          }}
+        />
         <Script id="yandex-metrika" strategy="afterInteractive">
           {`(function(m,e,t,r,i,k,a){
     m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
